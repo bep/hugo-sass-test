8b78cc1fb0901983bd77b6192a1194a71750fe52
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2018-10-04 19:20:09 +0200

    Fix CircleCI setup

--- ci-install-hugo.sh
+++ ci-install-hugo.sh
@@ -1,11 +1,10 @@
+#!/usr/bin/env bash
+
 HUGO_DOWNLOAD=hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
 
 set -x
 set -e
 
-# Install Hugo if not already cached or upgrade an old version.
-if [ ! -e $HOME/bin/hugo ] || ! [[ `$HOME/bin/hugo version` =~ v${HUGO_VERSION} ]]; then
-  wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_DOWNLOAD}
-  tar xvzf ${HUGO_DOWNLOAD} hugo
-  mv hugo $HOME/bin/hugo
-fi
+wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_DOWNLOAD}
+tar xvzf ${HUGO_DOWNLOAD} hugo
+sudo mv hugo /usr/local/bin
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 rm -rf public
 hugo -b https://temp.bep.is/hugoscss/ || exit -1;
 s3deploy -source=public/ -region=eu-west-1 -bucket=bep.is -distribution-id=E8OKNT7W9ZYZ2 -path temp/hugoscss -max-delete=100
