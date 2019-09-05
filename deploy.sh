a2cf5c36403590d4b0d51c91083f1b47f3e63417
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2019-09-05 16:47:22 +0200

    Misc

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 rm -rf public
 hugo config | grep cache
 hugo -b https://temp.bep.is/hugoscss/ || exit -1;
-s3deploy -source=public/ -region=eu-west-1 -bucket=bep.is -distribution-id=E8OKNT7W9ZYZ2 -path temp/hugoscss -max-delete=100
+s3deploy -source=public/ -region=eu-west-1 -bucket=bep.is -distribution-id=E8OKNT7W9ZYZ2 -path temp/hugoscss -max-delete=100 -public-access
